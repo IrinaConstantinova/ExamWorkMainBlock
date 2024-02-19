e750fbeda183fce3fdcce211d98fb5c50bcb393a
Irina.Constantinova <irina.constantinova@gmail.com>
2024-02-19 16:56:04 +0300

    Added changes to the program code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] FilterStrings(string[] inputArray)
+{
+    int filteredCount = 0;
+
+    foreach (string str in inputArray)
+    {
+        if (str.Length <= 3)
+        {
+            filteredCount++;
+        }
+    }
+
+    string[] filteredArray = new string[filteredCount];
+        int index = 0;
+
+    foreach (string str in inputArray)
+    {
+        if (str.Length <= 3)
+        {
+            filteredArray[index] = str;
+            index++;
+        }
+    }
+
+     return filteredArray;
+}
+
+void PrintArray(string[] array, string text)
+{
+    Console.WriteLine($"{text}: [{string.Join(", ", array)}]");
+}
+
+//----------------------------------------------------
+string[] inputArray = { "1234", "1567", "-2", "computer science" }; // Исходный массив задан изначально
+string[] filteredArray = FilterStrings(inputArray);
+
+PrintArray(inputArray, "Исходный массив");
+PrintArray(filteredArray, "Отфильтрованный массив");
